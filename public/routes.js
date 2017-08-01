2c938e5289eec0f5ee223ec333e7fa6fabe2fa0b
srinivasanvelu23 <ursfriendly_sri@yahoo.com>
2017-08-01 10:12:31 +0530

    Update routes.js

--- public/routes.js
+++ public/routes.js
@@ -1,4 +1,4 @@
-var path=require('path');
+  var path=require('path');
 module.exports = function (app, fs, absPath) {
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname+"/index.html"));
